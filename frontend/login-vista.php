7f71233cae74793a8d3a373b7528d39770f04a56
xCarlosDevx <78973956+xCarlosDevx@users.noreply.github.com>
2021-06-28 02:45:39 -0400

    Update login-vista.php

--- frontend/login-vista.php
+++ frontend/login-vista.php
@@ -2,13 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Login / Register Magtimus</title>
+    <title>Login </title>
     
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     
     
     <link rel="stylesheet" href="icon/style.css">
-    <link rel="stylesheet" href="./CSS/style.css">
+    <link rel="stylesheet" href="./css/style.css">
     
 </head>
 <body>
